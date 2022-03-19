4c1e27d45a2707bced6ea8686ea23152c740058d
Alexy de Almeida <alexx.da95@gmail.com>
2022-03-19 17:34:47 +0000

    progress...

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,100 +4,178 @@ import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 
+import javax.sound.midi.SysexMessage;
+
 public class Client {
-    public static void main(String args[]) {
-	// args[0] <- hostname of destination
-	if (args.length == 0) {
-	    System.out.println("java TCPClient hostname");
-	    System.exit(0);
-	}
-
-
-    /* LEITURA DE FICHEIRO
-    FileInputStream fis = new FileInputStream(new File(filename));
-    int nread;
-    byte [] buf = new byte[buffsize];
-    do {
-        nread = fis.read(buf);
-        if (nread > 0) dos.write(buf, 0, nread);
-    }
-    while (nread-> -1);
-    fis.close()
-    ss.close();
-    */
-
-
-
-	Socket s = null;
-	int serversocket = 7000;
-	try {
-	    // 1o passo
-	    s = new Socket(args[0], serversocket);
-
-	    System.out.println("SOCKET=" + s);
-	    // 2o passo
-	    DataInputStream in = new DataInputStream(s.getInputStream());
-	    DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-	    String texto = "";
-	    InputStreamReader input = new InputStreamReader(System.in);
-	    BufferedReader reader = new BufferedReader(input);
-	    new MessageReader(in);
-	    System.out.println("Introduza texto:");
-
-	    // 3o passo
-	    while (true) {
-		// READ STRING FROM KEYBOARD
-		try {
-		    texto = reader.readLine();
-		} catch (Exception e) {
+	public static void main(String args[]) {
+		// args[0] <- hostname of destination
+		if (args.length == 0) {
+			System.out.println("java TCPClient hostname");
+			System.exit(0);
 		}
 
-		// WRITE INTO THE SOCKET
-		out.writeUTF(texto);
-		out.flush();
+		/*
+		 * LEITURA DE FICHEIRO
+		 * FileInputStream fis = new FileInputStream(new File(filename));
+		 * int nread;
+		 * byte [] buf = new byte[buffsize];
+		 * do {
+		 * nread = fis.read(buf);
+		 * if (nread > 0) dos.write(buf, 0, nread);
+		 * }
+		 * while (nread-> -1);
+		 * fis.close()
+		 * ss.close();
+		 */
 
-	    }
-
-	} catch (UnknownHostException e) {
-	    System.out.println("Sock:" + e.getMessage());
-	    // nao conseguiu encontrar o hostname
-	} catch (EOFException e) {
-	    System.out.println("EOF:" + e.getMessage());
-	} catch (IOException e) {
-	    System.out.println("IO:" + e.getMessage());
-	} finally {
-	    if (s != null)
+		Socket s = null;
+		int serversocket = 7000;
 		try {
-		    s.close();
+			// 1o passo
+			s = new Socket(args[0], serversocket);
+
+			System.out.println("SOCKET=" + s);
+			// 2o passo
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+			String texto = "";
+			InputStreamReader input = new InputStreamReader(System.in);
+			BufferedReader reader = new BufferedReader(input);
+			new MessageReader(in);
+			System.out.println("Introduza texto:");
+
+			// 3o passo
+			while (true) {
+				// READ STRING FROM KEYBOARD
+				try {
+					texto = reader.readLine();
+				} 
+				catch (Exception e) {
+					System.out.println(e);
+				}
+
+				String[] command = texto.split(" ");
+				if (command[0].equals("UP")){
+					copyFileData(command[1], out);
+				}
+
+				// WRITE INTO THE SOCKET
+				out.writeUTF(texto);
+				out.flush();
+
+			}
+
+		} catch (UnknownHostException e) {
+			System.out.println("Sock:" + e.getMessage());
+			// nao conseguiu encontrar o hostname
+		} catch (EOFException e) {
+			System.out.println("EOF:" + e.getMessage());
 		} catch (IOException e) {
-		    System.out.println("close:" + e.getMessage());
+			System.out.println("IO:" + e.getMessage());
+		} finally {
+			if (s != null)
+				try {
+					s.close();
+				} catch (IOException e) {
+					System.out.println("close:" + e.getMessage());
+				}
 		}
 	}
-    }
+
+	private static void copyFileData(String fileToUpload, DataOutputStream dos) throws IOException {
+
+		InputStream fis = null;
+
+		try{
+			fis = new FileInputStream(new File(fileToUpload));
+			byte [] buf = new byte [1024];
+			int bytesRead;
+			
+			do {
+				bytesRead = fis.read(buf);
+				if (bytesRead > 0) dos.write(buf, 0, bytesRead);
+			} while (bytesRead > - 1);
+		}
+
+		catch(Exception e){
+			System.out.println(e);
+		}
+		
+		finally {
+			fis.close();
+			dos.close();
+		}
+	}
+
+	private void commandMenu() {
+		System.out.println("COMMAND FORMAT: OPCODE|ARGS");
+		System.out.println("authentication: AU <username> <password>\n");
+		System.out.println("modify password: PW <username> <new password>\n");
+		System.out.println("list SERVER files: LSs\n");
+		System.out.println("list CLIENT files: LSc\n");
+		System.out.println("change SERVER directory: CDs <new directory>\n");
+		System.out.println("change CLIENT directory: CDc <new directory>\n");
+		System.out.println("download file: DN <filename>\n");
+		System.out.println("upload file: UP <filename>");
+	}
 }
 
 class MessageReader extends Thread {
 	DataInputStream in;
 
-	public MessageReader (DataInputStream in)  {
-		this.in=in;
+	public MessageReader(DataInputStream in) {
+		this.in = in;
 		this.start();
 	}
-	//=============================
-    @Override
-	public void run(){
+
+	// =============================
+	@Override
+	public void run() {
 		String resposta;
-		try{
-			while(true){
+		try {
+			while (true) {
 				// READ FROM SOCKET
 				String data = in.readUTF();
 
 				// DISPLAY WHAT WAS READ
 				System.out.println("Received: " + data);
-                
+
 			}
-		}catch(EOFException e){System.out.println("EOF:" + e);
-		}catch(IOException e){System.out.println("IO:" + e);}
+		} catch (EOFException e) {
+			System.out.println("EOF:" + e);
+		} catch (IOException e) {
+			System.out.println("IO:" + e);
+		}
 	}
 }
+
+/*
+ * HEARTBEATS
+ * 
+ * 
+ * InetAdress ia = InetAdress.getByName("localhost");
+ * try (DatagramSocket ds = new DatagramSocket()){
+ * ds.setSoTimeout(1);
+ * int failedheartbeats = 0;
+ * white (failedheartbeats < maxfailedrounds){
+ * try{
+ * ByteArrayOutputStream baos = new ByteArrayOutputStream();
+ * DataInputStream dos = new DataInputStream(baos);
+ * dos.writeIt(count++);
+ * byte[] buf = baos.toByteArray();
+ * 
+ * DatagramPacket dp = new DatagramPacket(buf, buf.length, ia, 4000);
+ * ds.send(dp);
+ * 
+ * 
+ * byte [] rbuf = new byte[4096];
+ * DatagramPacket dr = new DatagramPacket(rbuf, rbuf.length);
+ * 
+ * ds.receive(dr);
+ * failedheartbeats = 0;
+ * 
+ * }
+ * }
+ * }
+ */
--- src/server/PrimaryServer.java
+++ src/server/PrimaryServer.java
@@ -1,5 +1,6 @@
 package server;
 
+
 import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -8,6 +9,8 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
 
 public class PrimaryServer {
 
@@ -55,6 +58,10 @@ class Connection extends Thread {
     ArrayList<Connection> connections;
     Socket clientSocket;
     int thread_number;
+    File currentDir, usersFile;
+    String username = "";
+    String password;
+    String directory;
 
     public Connection(Socket aClientSocket, int numero, ArrayList<Connection> connections) {
         thread_number = numero;
@@ -71,9 +78,15 @@ class Connection extends Thread {
             System.out.println("Connection:" + e.getMessage());
         }
     }
+
     //=============================
     @Override
-    public void run(){
+    public void run() {
+        
+        // SETUP
+        usersFile = new File("users.txt");
+
+        // LOOP
         String resposta;
         try {
             while (true) {
@@ -81,6 +94,12 @@ class Connection extends Thread {
                 String data = in.readUTF();
                 System.out.println("T[" + thread_number + "] Recebeu: " + data);
                 resposta = data.toUpperCase();
+
+                // servidor trata do pedido do cliente aqui
+                parseCommand(data);
+                
+
+                // alterar estas próximas linhas depois, nao é necessário o "c"
                 for (Connection c : connections) {
                     c.out.writeUTF(resposta);
                     c.out.flush();
@@ -94,4 +113,265 @@ class Connection extends Thread {
             this.connections.remove(this);
         }
     }
+
+    // TODO: command parsing function
+    private void parseCommand(String data) {
+        String [] command = data.split(" ");
+
+        // CLIENT AUTHENTICATION ----------------------------------- //
+        if (command[0].toUpperCase().equals("AU")){
+            if (this.username.isEmpty()){
+                if(find_user(command[1], command[2], usersFile)){
+                    this.username = command[1];
+                    this.password = command[2];
+                }
+                else System.out.println("Wrong username or password!");
+            }
+            else if (this.username.equals(command[0])) System.out.println("User already registered!");
+            else System.out.println("Error command AU");
+        }
+
+        // MODIFY PASSWORD ----------------------------------- //
+        else if(command[0].toUpperCase().equals("PW")){
+            if (!this.username.isEmpty()){
+                modify_user_info(this.username, this.password, this.directory, 1, command[1]);
+                
+                // TODO: tem que desconectar utilizador
+                
+                
+            }
+            else System.out.println("User not registered!");
+        }
+
+        // LIST SERVER DIRECTORY  ----------------------------------- //
+        else if (command[0].toUpperCase().equals("LSS")){
+            listServerDirectory();
+        }
+
+        else if (command[0].toUpperCase().equals("CDS")){
+            changeServerDirectory(command[1]);
+            modify_user_info(this.username, this.password, this.directory, 2, command[1]);
+        }
+
+        else if command[0].toUpperCase().equals("DN"){
+            
+        }
+        
+        else if command[0].toUpperCase().equals("UP"){
+            
+        }
+        return;
+    }
+
+    // TODO: check if user exists "in database"
+    private boolean find_user(String username, String password, File user_file) throws FileNotFoundException {
+        Scanner scan = new Scanner(user_file);
+        while (scan.hasNextLine()) {
+            String[] line = scan.nextLine().split(",");
+            if (line[0].equals(username) && line[1].equals(password)) return true;
+        }
+        return false;
+    }
+
+
+    Scanner scan = new Scanner(user_file);
+    StringBuffer buffer = new StringBuffer();
+        while(scan.hasNextLine())
+
+    {
+        buffer.append
+    }
+
+
+        while(scan.hasNextLine())
+
+    {
+        String[] line = scan.nextLine().split(",");
+        if (line[0].equals(username)) return true;
+    }
+
 }
+
+    // TCP
+
+    // TODO: function that handles client password update--> must disconnect user after calling this function and authenticate him again
+    private void modify_user_info(String username, String password, String user_file_path, int option, String command) throws IOException {
+
+        // command = password if option = 1
+        // command = new dir if option = 2
+
+        Scanner sc = new Scanner(new File(user_file_path));
+        StringBuffer buffer = new StringBuffer();
+        while (sc.hasNextLine()) buffer.append(sc.nextLine() + System.lineSeparator());
+        String fileContent = buffer.toString();
+        sc.close();
+
+        String oldLine = username + "," + password + "," + user_file_path;
+
+        // change password
+        if (option == 1) {
+            this.directory = System.getProperty("user.dir") + "/home";
+            String newLine = username + "," + command + "," + directory;
+        }
+        
+        // change directory
+        else if (option == 2){
+            this.directory = this.directory + "/" + command;
+            String newLine = username + "," + command + "," + directory;
+        }
+
+        // overwrite user info in users.txt
+
+  
+    }
+
+
+    // TODO: function that returns list of files in current directory (aka $ls)
+    private void listServerDirectory() {
+        if (currentDir.isDirectory()) {
+            // TODO: SYNCHRONISE!!! and expand
+            File[] paths = currentDir.listFiles();
+            for (File path : paths) {
+                System.out.println(path);
+            }
+        }
+    }
+
+    // TODO: function that handles remote directory navigation (aka $cd)
+    private void changeServerDirectory(String directory){
+        if (currentDir.isDirectory()) {
+            File[] paths = currentDir.listFiles();
+            for (File path : paths) {
+                if (path.toString().equals(directory) && path.isDirectory()){
+                    
+                }
+            }
+        }
+    }
+
+    // TODO: function that handles file download
+
+    // TODO: function that handles file upload
+
+    // UDP
+    // TODO: heartbeat function to other server
+
+    // TODO?: data replication
+}
+
+class FileAccess {
+    private String filename;
+    private int readers;
+    private int writers;
+
+    public FileAccess(String filename){
+        this.filename = filename;
+        this.readers = 0;
+        this.writers = 0;
+    }
+
+    public String getFilename(){
+        return filename;
+    }
+    public int getReaders(){
+        return readers;
+    }
+    public int getWriters(){
+        return writers;
+    }
+
+    public void addReader(){
+        readers++;
+    }
+    public void removeReader(){
+        readers--;
+    }
+    public void addWriter(){
+        writers++;
+    }
+    public void removeWriter(){
+        writers--;
+    }
+}
+
+class FileSystemManager {
+    // NOTE: allow concurrent file reading but not if that file is being written on be someone else
+    private ArrayList<FileAccess> accessList;
+
+    public FileSystemManager() {
+        accessList = new ArrayList<FileAccess>();
+    }
+
+    private synchronized FileAccess accessFile(String filename){
+        for (FileAccess f : accessList){
+            if (f.getFilename().equals(filename)) return f;
+        }
+        
+        FileAccess f = new FileAccess(filename);
+        accessList.add(f);
+        return f;
+    }
+
+    public void writeFileStart(String filename){
+        FileAccess f = accessFile(filename);
+        synchronized(f){
+            while (f.getReaders() > 0 || f.getWriters() > 0) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            f.addWriter();
+        }
+    }
+
+    public void writeFileEnd(String filename){
+        FileAccess f = accessFile(filename);
+        synchronized(f){
+            f.removeWriter();
+            this.notifyAll();
+        }
+    }
+
+    public void readFileStart(String filename){
+        FileAccess f = accessFile(filename);
+        synchronized(f){
+            while (f.getWriters() > 0) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            f.addReader();
+        }
+    }
+
+    public void readFileEnd(String filename){
+        FileAccess f = accessFile(filename);
+        synchronized(f){
+            f.removeReader();
+            this.notifyAll();
+        }
+    }
+}
+
+
+
+
+// heartbeat
+trg (datagramsocket ds) = new d« datagramdoscket(4000)
+    criar byte array input streams
+
+    byte buf [] = new byte[4096];
+    DatagramPacket dp = new DatagramPacket(buf, buf.length);
+    ds.receive(dp);
+    ByteArrayInputStream bais = new ByteArrayInputStream(buf, 0, dp, getLength());
+    DataInputStream dis = new DataInputStream(bais);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos)
+    
+cliente envia heartbeat
+servidor responde
